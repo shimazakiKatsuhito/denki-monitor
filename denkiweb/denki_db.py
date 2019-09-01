7099c6cc5afacfc414beb914819b773155372ece
Shimazaki Katsuhito <penpengthb@barque.or.tv>
2019-09-01 13:22:58 +0900

    add denkiweb sources

---
+++ denkiweb/denki_db.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import datetime, date, timedelta
+
+import psycopg2
+conn = None
+
+import logging
+logging.basicConfig(filename='denki_db.log',level=logging.ERROR)
+
+# denki_DBのオープン
+def open_db():
+    global conn
+    try:
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect("host=192.168.8.80 port=5432 dbname=denkidb user=denkiusr password=passwd00")
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't open Database\n")
+        exit()
+    finally:
+        logging.debug("Success: Open Database\n")
+
+# denki_DBのクローズ
+def close_db():
+    global conn
+    try:
+        # close the PostgreSQL server
+        if conn!=None:
+           conn.close()
+           conn=None
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't close Database\n")
+        exit()
+    finally:
+        logging.debug("Success: close Database\n")
+
+# denki_DBのテーブル作成
+def create_table():
+    global conn
+    cmd = ("CREATE TABLE instantaneous_power (id serial PRIMARY KEY,"
+           "datetime timestamp NOT NULL,"
+           "power integer);")
+    try:
+        cur = conn.cursor()
+        cur.execute(cmd)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't create table(instantaneous_power)\n")
+        exit()
+    finally:
+        logging.debug("Success: Create Table(instantaneous_power)\n")
+
+    cmd = ("CREATE TABLE integral_power (id serial PRIMARY KEY,"
+           "datetime timestamp NOT NULL,"
+           "power integer);")
+    try:
+        cur = conn.cursor()
+        cur.execute(cmd)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't create table(integral_power)\n")
+        exit()
+    finally:
+        logging.debug("Success: Create Table(integral_power)\n")
+
+# 瞬時電力の記録
+def insert_InstantaneousPower(datetime, power):
+    global conn
+    cmd = ("INSERT INTO instantaneous_power (datetime, power) "
+           "VALUES ('{dt}','{pwr}');".format(dt=datetime,pwr=power))
+    try:
+        cur = conn.cursor()
+        cur.execute(cmd)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't insert InstantaneousPower\n")
+        exit()
+    finally:
+        logging.debug("Success: InstantaneousPower\n")
+
+# 定時積算電力の記録
+def insert_IntegralPower(datetime, power):
+    global conn
+    cmd = ("INSERT INTO integral_power (datetime, power) "
+           "VALUES ('{dt}','{pwr}');".format(dt=datetime,pwr=power))
+    try:
+        cur = conn.cursor()
+        cur.execute(cmd)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't insert IntegralPower\n")
+        exit()
+    finally:
+        logging.debug("Success: IntegralPower\n")
+
+
+# テーブルの一覧を参照
+def print_all_tables():
+    global conn
+    cmd = ("select relname as TABLE_NAME from pg_stat_user_tables")
+    try:
+        cur = conn.cursor()
+        cur.execute(cmd)
+        rows = cur.fetchall()
+        conn.commit()
+        # print(rows)
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't get all_tables\n")
+        exit()
+    finally:
+        logging.debug("Success: print_all_tables\n")
+    return rows
+
+# カラム一覧を参照
+def print_colums(table_name):
+    global conn
+    cmd = ("select * from information_schema.columns where " + \
+           "table_catalog='denkidb' and " + \
+           "table_name='" + table_name + "' order by ordinal_position;")
+    try:
+        cur = conn.cursor()
+        cur.execute(cmd)
+        rows = cur.fetchall()
+        conn.commit()
+        # print(rows)
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't get all_tables\n")
+        exit()
+    finally:
+        logging.debug("Success: print_all_tables\n")
+    return rows
+
+# 瞬時電力の値を参照
+def print_InstantaneousPower():
+    global conn
+    cmd = ("SELECT * from instantaneous_power;")
+    try:
+        cur = conn.cursor()
+        cur.execute(cmd)
+        rows = cur.fetchall()
+        conn.commit()
+        # print(rows)
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't insert InstantaneousPower\n")
+        exit()
+    finally:
+        logging.debug("Success: InstantaneousPower\n") 
+    return rows
+
+# 今の瞬時電力の値を参照
+def get_InstantaneousPower_now():
+    global conn
+    cmd = ("SELECT * from instantaneous_power where datetime=(select max(datetime) from instantaneous_power);")
+    try:
+        cur = conn.cursor()
+        cur.execute(cmd)
+        row = cur.fetchone()
+        conn.commit()
+        # print(rows)
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't insert InstantaneousPower now\n")
+        exit()
+    finally:
+        logging.debug("Success: InstantaneousPower now\n") 
+    return row
+
+# 指定時間内の瞬時電力の値を参照
+def get_InstantaneousPower_period(starttime, endtime):
+    global conn
+    cmd = ("SELECT * from instantaneous_power where datetime between '"+starttime+"' and '"+endtime+"' order by datetime;")
+    try:
+        cur = conn.cursor()
+        cur.execute(cmd)
+        rows = cur.fetchall()
+        conn.commit()
+        # print(rows)
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't insert InstantaneousPower now\n")
+        exit()
+    finally:
+        logging.debug("Success: InstantaneousPower now\n") 
+    return rows
+
+# 定時積算電力の値を参照
+def print_IntegralPower():
+    global conn
+    cmd = ("SELECT * from integral_power;")
+    try:
+        cur = conn.cursor()
+        cur.execute(cmd)
+        rows = cur.fetchall()
+        conn.commit()
+        # print(rows)
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't insert InstantaneousPower\n")
+        exit()
+    finally:
+        logging.debug("Success: InstantaneousPower\n") 
+    return rows
+
+# 指定時間内の積算電力の値を参照
+def get_IntegralPower_period(starttime, endtime):
+    global conn
+    cmd = ("SELECT * from integral_power where datetime between '"+starttime+"' and '"+endtime+"' order by datetime;")
+    try:
+        cur = conn.cursor()
+        cur.execute(cmd)
+        rows = cur.fetchall()
+        conn.commit()
+        # print(rows)
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't insert integral_power\n")
+        exit()
+    finally:
+        logging.debug("Success: integral_power\n") 
+    return rows
+
+# 指定日の電気使用量を算出
+def get_SpecefiedDate_IntegralPower(date):
+    global conn
+  
+    try:
+        cur = conn.cursor()
+        # 指定日の積算電力の値
+        starttime = date + u" 00:00:05"
+        dt = datetime.strptime(date, '%Y-%m-%d')
+        endtime = (dt+timedelta(days=1)).strftime("%Y-%m-%d") + u" 00:00:05"
+        cmd = ("SELECT * from integral_power where datetime between '"+starttime+"' and '"+endtime+"' order by datetime DESC LIMIT 1;")
+        cur.execute(cmd)
+        row1 = cur.fetchone()
+        # 指定日前日の積算電力の値
+        starttime = (dt-timedelta(days=1)).strftime("%Y-%m-%d") + u" 00:00:05"
+        dt = datetime.strptime(date, '%Y-%m-%d')
+        endtime = date + u" 00:00:05"
+        cmd = ("SELECT * from integral_power where datetime between '"+starttime+"' and '"+endtime+"' order by datetime DESC LIMIT 1;")
+        cur.execute(cmd)
+        row2 = cur.fetchone()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't insert integral_power\n")
+        exit()
+    finally:
+        logging.debug("Success: integral_power\n") 
+    if (row1 is None) or (row2 is None):
+        logging.info("debug1")
+        return [ 0, dt, 0]
+    else:
+        logging.info("debug2")
+        return [row2[0], dt, row1[2]-row2[2]]
+    #return [row2[0], dt, row1[2]-row2[2]]
+
+# 最近一週間分の使用電力量の値を算出
+def get_NearWeek_IntegralPower():
+    #global conn
+    try:
+        rows = []
+        list1 = [6,5,4,3,2,1,0]
+        for d in list1:
+          date = (datetime.today()-timedelta(days=d)).strftime("%Y-%m-%d")
+          row = get_SpecefiedDate_IntegralPower(date)
+          rows.append(row)
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't insert integral_power\n")
+        exit()
+    finally:
+        logging.debug("Success: integral_power\n") 
+    return rows
+
+# 指定日からnum日分の毎日の使用電力量の値を算出
+def get_Daily_IntegralPower_fromSpecifiedDate(date, numday):
+    #global conn
+    try:
+        rows = []
+        dt = datetime.strptime(date, '%Y-%m-%d')
+        list1 = range(0, numday)
+        for d in list1:
+          date = (dt+timedelta(days=d)).strftime("%Y-%m-%d")
+          row = get_SpecefiedDate_IntegralPower(date)
+          rows.append(row)
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+        logging.error("Error: Can't insert integral_power\n")
+        exit()
+    finally:
+        logging.debug("Success: integral_power\n") 
+    return rows
+
+
+
+
---
+++ denkiweb/denkiweb.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import datetime, date, timedelta
+import calendar
+import _strptime
+
+import denki_db
+
+import logging
+logging.basicConfig(filename='denki_db.log',level=logging.ERROR)
+
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
+
+denki_db.open_db()
+ 
+@app.route('/')
+def index():
+    row = denki_db.get_InstantaneousPower_now()
+    datatime = row[1].strftime("%Y/%m/%d %H:%M")
+    datapower = row[2]
+    # index.html をレンダリングする
+    return render_template('index.html', nowpower=datapower, nowtime=datatime)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/today')
+def today_instantaneous_power():
+    today = datetime.today().strftime("%Y-%m-%d")
+    starttime = today + u" 00:00:00"
+    endtime = today + u" 23:59:59"
+    todaydata = denki_db.get_InstantaneousPower_period(starttime, endtime)
+
+    yesterday = (datetime.today()-timedelta(days=1)).strftime("%Y-%m-%d")
+    starttime = yesterday + u" 00:00:00"
+    endtime = yesterday + u" 23:59:59"
+    yesterdaydata = denki_db.get_InstantaneousPower_period(starttime, endtime)
+
+    row = denki_db.get_InstantaneousPower_now()
+    datatime = row[1].strftime("%Y/%m/%d %H:%M")
+    datapower = row[2]
+
+    # index.html をレンダリングする
+    return render_template('index.html', graphdata=todaydata, graphdata2=yesterdaydata, nowpower=datapower, nowtime=datatime)
+
+@app.route('/week')
+def thisweek_integral_power():
+    wdata = denki_db.get_NearWeek_IntegralPower()
+
+    row = denki_db.get_InstantaneousPower_now()
+    datatime = row[1].strftime("%Y/%m/%d %H:%M")
+    datapower = row[2]
+
+    # index.html をレンダリングする
+    return render_template('index.html', graphdata3=wdata, nowpower=datapower, nowtime=datatime, refresh_rate=60*10)
+
+@app.route('/SpecefiedDate/<date>')
+def get_SpecefiedDate_IntegralPower(date=None):
+    data = denki_db.get_SpecefiedDate_IntegralPower(date)
+    msg=date+u"の電気使用量は、"+str(data[2]/10.0)+u"[kWh]です。"
+
+    row = denki_db.get_InstantaneousPower_now()
+    datatime = row[1].strftime("%Y/%m/%d %H:%M")
+    datapower = row[2]
+
+    # index.html をレンダリングする
+    return render_template('index.html', message=msg, nowpower=datapower, nowtime=datatime, refresh_rate=60*10)
+
+@app.route('/dailythismonth')
+def get_deilythismonth_integral_power():
+    mstart = (datetime.today().replace(day=1)).strftime("%Y-%m-%d")
+    mdays = calendar.monthrange(datetime.today().year, datetime.today().month)[1]
+    pdata = denki_db.get_Daily_IntegralPower_fromSpecifiedDate(mstart,mdays)
+
+    wdata = []
+    day = 0
+    # 第1週目
+    w1data = []
+    wd = pdata[0][1].weekday()
+    for i in range(0, wd):
+      w1data.append([0,None,0])
+    for i in range(wd, 7):
+      w1data.append(pdata[day])
+      day+=1
+    wdata.append(w1data)
+    # 第2週目以降
+    for n in range(1,5):
+      w2data = []
+      for i in range(7):
+        if(day<mdays):
+          w2data.append(pdata[day])
+          day+=1
+        else:
+          w2data.append([0,None,0])
+      wdata.append(w2data)
+
+    row = denki_db.get_InstantaneousPower_now()
+    datatime = row[1].strftime("%Y/%m/%d %H:%M")
+    datapower = row[2]
+
+    # index.html をレンダリングする
+    return render_template('index.html', graphdata4=wdata, nowpower=datapower, nowtime=datatime, refresh_rate=60*60)
+
+
+if __name__ == "__main__":
+    app.debug = True # デバッグモード有効化
+    app.run(host='0.0.0.0', port=5000) # どこからでもアクセス可能に
